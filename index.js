45abacbd26746cf71b4404dde7fc96afa77eb912
Kushal Pathak <84308717+Kushal-Pathak@users.noreply.github.com>
2022-04-27 16:23:37 +0545

    Add files via upload

--- index.js
+++ index.js
@@ -1,24 +1,41 @@
-
-for(let i=0;i<document.querySelectorAll(".drum").length;i++)
-{
-    document.querySelectorAll("button")[i].addEventListener("click", 
-    
-    function ()
-    {
-        var audio = new Audio("sounds/"+i+".mp3");
-        audio.play();
-        
-        
-
+for (let i = 0; i < document.querySelectorAll(".drum").length; i++) {
+  document.querySelectorAll("button")[i].addEventListener(
+    "click",
+    function () {
+      var audio = new Audio("sounds/" + i + ".mp3");
+      audio.play();
     }
-    );
+  );
 }
 
-document.addEventListener("keypress", 
-    function (event) {
-        if (event == "w")
-        {
-            var audio = new Audio("sounds/0.mp3");
-            audio.play();
-        }
-    });
+document.addEventListener("keypress", function (e) {
+    if(e.key === 'w'){
+        var audio = new Audio("sounds/0.mp3");
+        audio.play();
+    }
+    if(e.key === 'a'){
+        var audio = new Audio("sounds/1.mp3");
+        audio.play();
+    }
+    if(e.key === 's'){
+        var audio = new Audio("sounds/2.mp3");
+        audio.play();
+    }
+    if(e.key === 'd'){
+        var audio = new Audio("sounds/3.mp3");
+        audio.play();
+    }
+    if(e.key === 'j'){
+        var audio = new Audio("sounds/4.mp3");
+        audio.play();
+    }
+    if(e.key === 'k'){
+        var audio = new Audio("sounds/5.mp3");
+        audio.play();
+    }
+    if(e.key === 'l'){
+        var audio = new Audio("sounds/6.mp3");
+        audio.play();
+    }
+
+});
